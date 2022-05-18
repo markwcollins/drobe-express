496025cb1577b8396ac56e867120792362b0e85a
“Mark <mark.william.collins@gmail.com>
2022-05-18 12:04:04 +1000

    updates

--- crons/index.ts
+++ crons/index.ts
@@ -5,25 +5,25 @@ import schedule from 'node-schedule'
 
 export const initCrons = () => {
 
-  const updateFXRatesCronRule = new schedule.RecurrenceRule()
-  updateFXRatesCronRule.dayOfWeek = [1, 2, 3, 4, 5] // every weekday
-  updateFXRatesCronRule.hour = 2 // utc time
-  updateFXRatesCronRule.minute = 10
+  // const updateFXRatesCronRule = new schedule.RecurrenceRule()
+  // updateFXRatesCronRule.dayOfWeek = [1, 2, 3, 4, 5] // every weekday
+  // updateFXRatesCronRule.hour = 2 // utc time
+  // updateFXRatesCronRule.minute = 10
 
-  schedule.scheduleJob(updateFXRatesCronRule, function() {
-    console.log('starting updateFXRatesCronRule')
-    const updateFXRatesCron = new UpdateFXRatesCron()
-    updateFXRatesCron.run()
-  })
+  // schedule.scheduleJob(updateFXRatesCronRule, function() {
+  //   console.log('starting updateFXRatesCronRule')
+  //   const updateFXRatesCron = new UpdateFXRatesCron()
+  //   updateFXRatesCron.run()
+  // })
 
-  const updateWebPagesCronRule = new schedule.RecurrenceRule()
-  updateWebPagesCronRule.dayOfWeek = [0, 1, 2, 3, 4, 5, 6] // every day
-  updateWebPagesCronRule.hour = 2 // utc time
-  updateWebPagesCronRule.minute = 0
+  // const updateWebPagesCronRule = new schedule.RecurrenceRule()
+  // updateWebPagesCronRule.dayOfWeek = [0, 1, 2, 3, 4, 5, 6] // every day
+  // updateWebPagesCronRule.hour = 2 // utc time
+  // updateWebPagesCronRule.minute = 0
   
-  schedule.scheduleJob(updateWebPagesCronRule, function() {
-    const updateWebPagesByUserCron = new UpdateWebPagesByUserCron()
-    console.log('starting UpdateWebPagesByUserCron')
-    updateWebPagesByUserCron.run()
-  })
+  // schedule.scheduleJob(updateWebPagesCronRule, function() {
+  //   const updateWebPagesByUserCron = new UpdateWebPagesByUserCron()
+  //   console.log('starting UpdateWebPagesByUserCron')
+  //   updateWebPagesByUserCron.run()
+  // })
 }
